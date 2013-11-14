34b2e1514fbcf85166243acbc56ea276c35d21cf
Callum <callum@callumj.co.uk>
2013-11-14 00:14:00 +0000

    Only run if Twitter name matches

--- scripts/twitter.js
+++ scripts/twitter.js
@@ -1,36 +1,43 @@
 function Twitter(config) {
 
-  document.querySelector(".profile-picture .avatar").classList.add("fadeout");
+  var regexp = new RegExp(config.name, "i");
+  var fullName = document.querySelector(".fullname .profile-field");
 
-  window.setTimeout(function() {
-    lineThrough(document.querySelector(".fullname .profile-field"));
-  }, 600);
+  if (regexp.test(fullName.innerHTML)) {
 
-  window.setTimeout(function() {
-    lineThrough(document.querySelector(".username .screen-name"));
-  }, 800);
+    document.querySelector(".profile-picture .avatar").classList.add("fadeout");
 
-  window.setTimeout(function() {
-    lineThrough(document.querySelector(".bio"));
-  }, 1000);
+    window.setTimeout(function() {
+      lineThrough(fullName);
+    }, 600);
 
-  window.setTimeout(function() {
-    lineThrough(document.querySelector(".location-and-url .location"));
-  }, 1200);
+    window.setTimeout(function() {
+      lineThrough(document.querySelector(".username .screen-name"));
+    }, 800);
 
-  window.setTimeout(function() {
-    lineThrough(document.querySelector(".location-and-url .url .profile-field"));
-  }, 1400);
+    window.setTimeout(function() {
+      lineThrough(document.querySelector(".bio"));
+    }, 1000);
 
-  killCount(document.querySelector(
-    "[data-element-term=tweet_stats] strong"));
-  killCount(document.querySelector(
-    "[data-element-term=following_stats] strong"));
-  killCount(document.querySelector(
-    "[data-element-term=follower_stats] strong"));
+    window.setTimeout(function() {
+      lineThrough(document.querySelector(".location-and-url .location"));
+    }, 1200);
+
+    window.setTimeout(function() {
+      lineThrough(document.querySelector(".location-and-url .url .profile-field"));
+    }, 1400);
+
+    killCount(document.querySelector(
+      "[data-element-term=tweet_stats] strong"));
+    killCount(document.querySelector(
+      "[data-element-term=following_stats] strong"));
+    killCount(document.querySelector(
+      "[data-element-term=follower_stats] strong"));
+
+  }
 
   killElements({
-    regexp: new RegExp(config.name, "i"),
+    regexp: regexp,
     elements: document.querySelectorAll(".js-navigable-stream .js-stream-item")
   });
 
